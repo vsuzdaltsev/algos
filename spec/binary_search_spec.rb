96e64718099b1189f1ac90f81a94f580b731e1d3
Your Name <you@example.com>
2019-03-03 22:09:47 +0300

    spec

---
+++ spec/binary_search_spec.rb
@@ -0,0 +1,11 @@
+describe 'BinarySearch.search' do
+  context "when search in sorted array" do
+    it "should return index of element" do
+      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+      what_to_find = 77
+      index        = 6
+      
+      expect(BinarySearch.run(array, what_to_find)).to eq index
+    end
+  end
+end
