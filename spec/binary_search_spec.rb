d0fb6d342e20d1fae3331326a8f65db74d03aec8
Vsevolod Suzdaltsev <vsuzdaltsev@example.com>
2019-03-07 14:51:51 +0000

    specs extended

--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -1,9 +1,29 @@
 describe 'BinarySearch.search' do
-  context "when search in sorted array" do
+  context "when search 6th element in sorted array" do
     it "should return index of element" do
       array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
       what_to_find = 77
       index        = 6
+
+      expect(BinarySearch.run(array, what_to_find)).to eq index
+    end
+  end
+
+  context "when search first element in sorted array" do
+    it "should return index of element" do
+      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+      what_to_find = 11
+      index        = 0
+
+      expect(BinarySearch.run(array, what_to_find)).to eq index
+    end
+  end
+
+  context "when search last element in sorted array" do
+    it "should return index of element" do
+      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+      what_to_find = 111
+      index        = 9
 
       expect(BinarySearch.run(array, what_to_find)).to eq index
     end
