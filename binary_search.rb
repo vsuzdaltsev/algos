d158b8f1403e1d4e5ca9b09804ee4cd043e31c4a
Your Name <you@example.com>
2019-03-09 21:18:47 +0300

    added strings array + specs

--- binary_search.rb
+++ binary_search.rb
@@ -1,5 +1,5 @@
 class BinarySearch
-  def self.random_array(size: 0, max: 0)
+  def self.random_int_array(size: 0, max: 0)
     arr = []
     while arr.size < size
       elem = rand(max)
@@ -8,15 +8,28 @@ class BinarySearch
     arr.sort
   end
 
+  def self.random_str_array(size: 0, str_len: 0)
+    size.times.map.each_with_object([]) do |_t, memo|
+      memo << BinarySearch.random_str(str_len: str_len)
+    end.sort
+  end
+
+  def self.random_str(str_len: 0)
+    str_len.times.map.each_with_object('') do |_t, memo|
+      memo << ('a'..'z').to_a.sample
+    end
+  end
+
   def self.run(arr, item, low = 0, high = arr.size - 1)
     while low <= high do
       mid   = (low + high) / 2
       guess = arr[mid]
       return mid if guess.eql?(item)
 
-      low  = mid + 1 if guess < item
-      high = mid - 1 if guess > item
+      low  = mid + 1 if (guess <=> item) == -1
+      high = mid - 1 if (guess <=> item) == 1
     end
     nil
   end
+
 end
--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -1,41 +1,85 @@
+module Helper
+  IntArray = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+  StrArray = %w[depfm fayik ggfhj hywle ohngr skjlz toedw wziyq xdpgm ylzyo]
+end
+
 describe 'BinarySearch.search' do
-  context "when search 6th element in sorted array" do
-    it "should return index of element" do
-      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
-      what_to_find = 77
-      index        = 6
+  context 'when array consists of Integer elements' do
+    context "when search 6th element in sorted array" do
+      it "should return index of element" do
+        what_to_find = 77
+        index        = 6
 
-      expect(BinarySearch.run(array, what_to_find)).to eq index
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
+    end
+
+    context "when search first element in sorted array" do
+      it "should return index of element" do
+        array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+        what_to_find = 11
+        index        = 0
+
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
+    end
+
+    context "when search last element in sorted array" do
+      it "should return index of element" do
+        array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+        what_to_find = 111
+        index        = 9
+
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
+    end
+
+    context "when search nonexistent element in sorted array" do
+      it "should return nil" do
+        array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
+        what_to_find = 222
+        index        = nil
+
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
     end
   end
 
-  context "when search first element in sorted array" do
-    it "should return index of element" do
-      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
-      what_to_find = 11
-      index        = 0
+  context 'when array consists of String elements' do 
+    context "when search 6th element in sorted array" do
+      it "should return index of element" do
+        what_to_find = 77
+        index        = 6
 
-      expect(BinarySearch.run(array, what_to_find)).to eq index
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
     end
-  end
 
-  context "when search last element in sorted array" do
-    it "should return index of element" do
-      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
-      what_to_find = 111
-      index        = 9
+    context "when search first element in sorted array" do
+      it "should return index of element" do
+        what_to_find = 11
+        index        = 0
 
-      expect(BinarySearch.run(array, what_to_find)).to eq index
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
     end
-  end
 
-  context "when search nonexistent element in sorted array" do
-    it "should return nil" do
-      array        = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111]
-      what_to_find = 222 
-      index        = nil
+    context "when search last element in sorted array" do
+      it "should return index of element" do
+        what_to_find = 111
+        index        = 9
 
-      expect(BinarySearch.run(array, what_to_find)).to eq index
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
+    end
+
+    context "when search nonexistent element in sorted array" do
+      it "should return nil" do
+        what_to_find = 222 
+        index        = nil
+
+        expect(BinarySearch.run(Helper::IntArray, what_to_find)).to eq index
+      end
     end
   end
 end
