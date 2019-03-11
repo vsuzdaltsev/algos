89e286b87951655723ed69fbd2ad0b9a1d785627
Vsevolod Suzdaltsev <vsuzdaltsev@example.com>
2019-03-11 16:11:49 +0300

    refactor specs

--- spec/binary_search_spec.rb
+++ spec/binary_search_spec.rb
@@ -1,92 +1,60 @@
-module SortedArrays
-  INT = [11, 22, 33, 44, 55, 66, 77, 88, 99, 111].freeze
-  STR = %w[
-    depfm
-    fayik
-    ggfhj
-    hywle
-    ohngr
-    skjlz
-    toedw
-    wziyq
-    xdpgm
-    ylzyo
-  ].freeze
-end
-
-describe 'BinarySearch.search' do
+describe 'BinarySearch.run' do
   context 'when array consists of Integer elements' do
+    before do
+      @arr = Random.int_array(size: 1000, max: 10000).freeze
+    end
+
     context 'when search for the 6th element in sorted array' do
       it 'should return index of element' do
-        what_to_find = 77
-        index        = 6
-
-        expect(BinarySearch.run(SortedArrays::INT, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, @arr[6])).to eq 6
       end
     end
 
     context 'when search for the first element in sorted array' do
       it 'should return index of element' do
-        what_to_find = 11
-        index        = 0
-
-        expect(BinarySearch.run(SortedArrays::INT, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, @arr[0])).to eq 0
       end
     end
 
     context 'when search for the last element in sorted array' do
       it 'should return index of element' do
-        what_to_find = 111
-        index        = 9
-
-        expect(BinarySearch.run(SortedArrays::INT, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, @arr[555])).to eq 555
       end
     end
 
     context 'when search for the nonexistent element in sorted array' do
       it 'should return nil' do
-        what_to_find = 222
-        index        = nil
-
-        expect(BinarySearch.run(SortedArrays::INT, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, 99999999)).to eq nil
       end
     end
   end
 
   context 'when array consists of String elements' do
+    before do
+      @arr = Random.str_array(size: 10000, str_len: 10).freeze
+    end
+
     context 'when search for the 6th element in sorted array' do
       it 'should return index of element' do
-        what_to_find = 'toedw' 
-        index        = 6
-
-        expect(BinarySearch.run(SortedArrays::STR, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, @arr[6])).to eq 6
       end
     end
 
     context 'when search for the first element in sorted array' do
       it 'should return index of element' do
-        what_to_find = 'depfm' 
-        index        = 0
-
-        expect(BinarySearch.run(SortedArrays::STR, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, @arr[0])).to eq 0
       end
     end
 
-    context 'when search for the last element in sorted array' do
+    context 'when search for the 555th element in sorted array' do
       it 'should return index of element' do
-        what_to_find = 'ylzyo' 
-        index        = 9
-
-        expect(BinarySearch.run(SortedArrays::STR, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, @arr[555])).to eq 555
       end
     end
 
     context 'when search for the nonexistent element in sorted array' do
       it 'should return nil' do
-        what_to_find = 'nonexistent'
-        index        = nil
-
-        expect(BinarySearch.run(SortedArrays::STR, what_to_find)).to eq index
+        expect(BinarySearch.run(@arr, 'nonexistent')).to eq nil
       end
     end
   end
