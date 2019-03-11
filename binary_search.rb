55cfca464d0129e03fb63f8aeabafcc9136d731d
Vsevolod Suzdaltsev <vsuzdaltsev@example.com>
2019-03-11 12:24:59 +0000

    extract Random class

--- binary_search.rb
+++ binary_search.rb
@@ -1,7 +1,7 @@
-# Binary search
-class BinarySearch
+# Random sorted arrays
+class Random
   class << self
-    def random_int_array(size: 0, max: 0)
+    def int_array(size: 0, max: 0)
       arr = []
       while arr.size < size
         elem = rand(max)
@@ -10,12 +10,25 @@ class BinarySearch
       arr.sort
     end
 
-    def random_str_array(size: 0, str_len: 0)
+    def str_array(size: 0, str_len: 0)
       size.times.map.each_with_object([]) do |_t, memo|
         memo << random_str(str_len: str_len)
       end.sort
     end
 
+    private
+
+    def random_str(str_len: 0)
+      str_len.times.map.each_with_object('') do |_t, memo|
+        memo << ('a'..'z').to_a.sample
+      end
+    end
+  end
+end
+
+# Binary search
+class BinarySearch
+  class << self
     def run(arr, item, low = 0, high = arr.size - 1)
       while low <= high
         mid   = (low + high) / 2
@@ -26,14 +39,6 @@ class BinarySearch
         high = mid - 1 if (guess <=> item).eql?(1)
       end
       nil
-    end
-
-    private
-
-    def random_str(str_len: 0)
-      str_len.times.map.each_with_object('') do |_t, memo|
-        memo << ('a'..'z').to_a.sample
-      end
     end
   end
 end
